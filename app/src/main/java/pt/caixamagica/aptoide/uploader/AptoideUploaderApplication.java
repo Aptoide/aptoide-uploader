c161dfdc5ed9ebafcc7f86345c85eec89962abdb
Neurophobic Animal <FredericoSantinho@gmail.com>
2017-10-13 15:49:03 +0100

    [AAO-74] - Added preliminary md5 logic.

--- app/src/main/java/pt/caixamagica/aptoide/uploader/AptoideUploaderApplication.java
+++ app/src/main/java/pt/caixamagica/aptoide/uploader/AptoideUploaderApplication.java
@@ -9,9 +9,22 @@ import android.app.Application;
 import android.content.Context;
 import com.crashlytics.android.Crashlytics;
 import com.facebook.AppEventsLogger;
+import com.octo.android.robospice.SpiceManager;
+import com.octo.android.robospice.persistence.exception.SpiceException;
+import com.octo.android.robospice.request.listener.RequestListener;
 import io.fabric.sdk.android.Fabric;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 import lombok.Getter;
 import lombok.Setter;
+import pt.caixamagica.aptoide.uploader.retrofit.RetrofitSpiceServiceUploaderSecondary;
+import pt.caixamagica.aptoide.uploader.retrofit.request.UploadedAppsRequest;
+import pt.caixamagica.aptoide.uploader.util.InstalledUtils;
+import pt.caixamagica.aptoide.uploader.util.Md5AsyncUtils;
+import pt.caixamagica.aptoide.uploader.util.StoredCredentialsManager;
+import pt.caixamagica.aptoide.uploader.webservices.json.UserCredentialsJson;
 
 /**
  * Created by neuro on 23-03-2015.
@@ -22,6 +35,8 @@ public class AptoideUploaderApplication extends Application {
   private static AptoideUploaderApplication context;
   @Getter @Setter private static boolean forcedLogout = false;
   @Getter @Setter private String username;
+  private double BUFFER_SIZE = 5;
+  private SpiceManager spiceManager;
 
   public static Context getContext() {
     return context;
@@ -32,5 +47,77 @@ public class AptoideUploaderApplication extends Application {
     Fabric.with(this, new Crashlytics());
     context = this;
     AppEventsLogger.activateApp(this);
+    spiceManager = new SpiceManager(RetrofitSpiceServiceUploaderSecondary.class);
+    spiceManager.start(this);
+    refreshInstalledAppsMd5List();
+  }
+
+  private void refreshInstalledAppsMd5List() {
+    Md5AsyncUtils md5AsyncUtils = new Md5AsyncUtils(this);
+    InstalledUtils installedUtils = new InstalledUtils(this);
+
+    List<SelectablePackageInfo> selectablePackageInfos = installedUtils.nonSystemPackages(false);
+    md5AsyncUtils.computeMd5(selectablePackageInfos,
+        createMd5Listener(selectablePackageInfos.size()));
+  }
+
+  private Md5AsyncUtils.OnNewUploadedApps createMd5Listener(final int size) {
+    return new Md5AsyncUtils.OnNewUploadedApps() {
+      private AtomicInteger atomicInteger = new AtomicInteger(0);
+      private ConcurrentLinkedQueue<Md5AsyncUtils.Model> concurrentLinkedQueue =
+          new ConcurrentLinkedQueue<>();
+
+      @Override public void onNewUploadedApps(Md5AsyncUtils.Model model) {
+        int count = atomicInteger.incrementAndGet();
+        if (count % BUFFER_SIZE == 0 || count == size) {
+          List<Md5AsyncUtils.Model> modelList = createModelList();
+          sendUploadedAppsRequest(modelList);
+        } else {
+          concurrentLinkedQueue.add(model);
+        }
+      }
+
+      private List<Md5AsyncUtils.Model> createModelList() {
+        List<Md5AsyncUtils.Model> list = new LinkedList<>();
+
+        for (int i = 0; i < BUFFER_SIZE; i++) {
+          list.add(concurrentLinkedQueue.poll());
+        }
+
+        return list;
+      }
+
+      private void sendUploadedAppsRequest(List<Md5AsyncUtils.Model> modelList) {
+        UserCredentialsJson storedUserCredentials =
+            new StoredCredentialsManager(getContext()).getStoredUserCredentials();
+
+        String token = storedUserCredentials.getToken();
+        String storeName = storedUserCredentials.getRepo();
+
+        spiceManager.execute(
+            new UploadedAppsRequest(token, storeName, buildInstalledAppsMd5String(modelList)),
+            new RequestListener<UserCredentialsJson>() {
+              @Override public void onRequestFailure(SpiceException spiceException) {
+                spiceException.printStackTrace();
+              }
+
+              @Override public void onRequestSuccess(UserCredentialsJson userCredentialsJson) {
+
+              }
+            });
+      }
+    };
+  }
+
+  private String buildInstalledAppsMd5String(List<Md5AsyncUtils.Model> modelList) {
+    String str = "[";
+
+    for (Md5AsyncUtils.Model model : modelList) {
+      str += model;
+    }
+
+    str += "]";
+
+    return str;
   }
 }
