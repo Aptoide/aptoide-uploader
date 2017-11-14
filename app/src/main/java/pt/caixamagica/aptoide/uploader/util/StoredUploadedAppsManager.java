c6c0181207e192eae25ba6612b8570bf59733467
FilipeGonçalves <filisgoncalves@gmail.com>
2017-11-14 17:01:57 +0000

    [AAO-74] Created entity to manage the persistence of apps that are uploaded in the store on shared preferences

---
+++ app/src/main/java/pt/caixamagica/aptoide/uploader/util/StoredUploadedAppsManager.java
@@ -0,0 +1,34 @@
+package pt.caixamagica.aptoide.uploader.util;
+
+import android.content.SharedPreferences;
+import java.util.List;
+import pt.caixamagica.aptoide.uploader.UploadedApp;
+
+/**
+ * Created by filipe on 13-11-2017.
+ */
+
+public class StoredUploadedAppsManager {
+
+  private final SharedPreferences sharedPreferences;
+
+  public StoredUploadedAppsManager(SharedPreferences sharedPreferences) {
+    this.sharedPreferences = sharedPreferences;
+  }
+
+  public void saveUploadedApps(List<UploadedApp> listOfStoreApps) {
+    for (int i = 0; i < listOfStoreApps.size(); i++) {
+      addAppToSharedPreferences(listOfStoreApps.get(i));
+    }
+  }
+
+  private void addAppToSharedPreferences(UploadedApp app) {
+    sharedPreferences.edit()
+        .putLong(app.getPackageName(), app.getVercode())
+        .apply();
+  }
+
+  public boolean isAppInStore(String appName, long versionCode) {
+    return sharedPreferences.getLong(appName, Long.MAX_VALUE) == versionCode;
+  }
+}
