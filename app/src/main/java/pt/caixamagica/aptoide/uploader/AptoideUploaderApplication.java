a4c5ea18221d9c9538ad17f97a688a5bf5fe1e18
FilipeGonçalves <filisgoncalves@gmail.com>
2017-11-16 15:23:03 +0000

    [AAO-74] Created AppInStoreController to manage requesting if apps are in store

--- app/src/main/java/pt/caixamagica/aptoide/uploader/AptoideUploaderApplication.java
+++ app/src/main/java/pt/caixamagica/aptoide/uploader/AptoideUploaderApplication.java
@@ -11,24 +11,14 @@ import android.support.multidex.MultiDex;
 import com.crashlytics.android.Crashlytics;
 import com.facebook.AppEventsLogger;
 import com.octo.android.robospice.SpiceManager;
-import com.octo.android.robospice.persistence.exception.SpiceException;
-import com.octo.android.robospice.request.listener.RequestListener;
 import io.fabric.sdk.android.Fabric;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.atomic.AtomicInteger;
 import lombok.Getter;
 import lombok.Setter;
 import pt.caixamagica.aptoide.uploader.retrofit.RetrofitSpiceServiceUploaderSecondary;
-import pt.caixamagica.aptoide.uploader.retrofit.request.UploadedAppsRequest;
 import pt.caixamagica.aptoide.uploader.util.AppsInStorePersister;
 import pt.caixamagica.aptoide.uploader.util.InstalledUtils;
 import pt.caixamagica.aptoide.uploader.util.Md5AsyncUtils;
 import pt.caixamagica.aptoide.uploader.util.StoredCredentialsManager;
-import pt.caixamagica.aptoide.uploader.webservices.json.UploadedAppsJson;
-import pt.caixamagica.aptoide.uploader.webservices.json.UserCredentialsJson;
 
 /**
  * Created by neuro on 23-03-2015.
@@ -39,12 +29,10 @@ public class AptoideUploaderApplication extends Application {
   private static AptoideUploaderApplication context;
   @Getter @Setter private static boolean forcedLogout = false;
   @Getter @Setter private String username;
-  private double BUFFER_SIZE = 5;
-  private SpiceManager spiceManager;
   private StoredCredentialsManager storedCredentialsManager;
-  private List<UploadedApp> listOfAppsInStore;
-  private AppsInStorePersister appsInStorePersister;
   public static final String SHARED_PREFERENCES_FILE = "UploaderPrefs2";
+
+  private AppsInStoreController appsInStoreController;
 
   public static Context getContext() {
     return context;
@@ -63,108 +51,19 @@ public class AptoideUploaderApplication extends Application {
     storedCredentialsManager = new StoredCredentialsManager(this.getApplicationContext());
 
     if (isUserLoggedIn()) {
-      appsInStorePersister = new AppsInStorePersister(this.getApplicationContext()
-          .getSharedPreferences(SHARED_PREFERENCES_FILE, Context.MODE_PRIVATE));
-      listOfAppsInStore = new ArrayList<>();
-      spiceManager = new SpiceManager(RetrofitSpiceServiceUploaderSecondary.class);
-      spiceManager.start(this);
-      refreshInstalledAppsMd5List();
+      AppsInStorePersister appsInStorePersister = new AppsInStorePersister(
+          this.getApplicationContext()
+              .getSharedPreferences(SHARED_PREFERENCES_FILE, Context.MODE_PRIVATE));
+
+      InstalledUtils installedUtils = new InstalledUtils(this, appsInStorePersister);
+
+      appsInStoreController =
+          new AppsInStoreController(new SpiceManager(RetrofitSpiceServiceUploaderSecondary.class),
+              appsInStorePersister, installedUtils, new Md5AsyncUtils(this, installedUtils),
+              getApplicationContext());
+
+      appsInStoreController.start();
     }
-  }
-
-  private void refreshInstalledAppsMd5List() {
-    InstalledUtils installedUtils = new InstalledUtils(this, appsInStorePersister);
-    Md5AsyncUtils md5AsyncUtils = new Md5AsyncUtils(this, installedUtils);
-
-    List<SelectablePackageInfo> selectablePackageInfos = installedUtils.nonSystemPackages(false);
-    md5AsyncUtils.computeMd5(selectablePackageInfos,
-        createMd5Listener(selectablePackageInfos.size()));
-  }
-
-  private Md5AsyncUtils.OnNewUploadedApps createMd5Listener(final int size) {
-    return new Md5AsyncUtils.OnNewUploadedApps() {
-      private AtomicInteger atomicInteger = new AtomicInteger(0);
-      private ConcurrentLinkedQueue<Md5AsyncUtils.Model> concurrentLinkedQueue =
-          new ConcurrentLinkedQueue<>();
-
-      @Override public void onNewUploadedApps(Md5AsyncUtils.Model model) {
-        int count = atomicInteger.incrementAndGet();
-        if (count % BUFFER_SIZE == 0 || count == size) {
-          List<Md5AsyncUtils.Model> modelList = createModelList();
-          sendUploadedAppsRequest(modelList);
-        } else {
-          concurrentLinkedQueue.add(model);
-        }
-      }
-
-      private List<Md5AsyncUtils.Model> createModelList() {
-        List<Md5AsyncUtils.Model> list = new LinkedList<>();
-
-        for (int i = 0; i < Math.min(BUFFER_SIZE, concurrentLinkedQueue.size()); i++) {
-          if (!concurrentLinkedQueue.isEmpty()) {
-            list.add(concurrentLinkedQueue.poll());
-          }
-        }
-
-        return list;
-      }
-
-      private void sendUploadedAppsRequest(List<Md5AsyncUtils.Model> modelList) {
-        UserCredentialsJson storedUserCredentials =
-            new StoredCredentialsManager(getContext()).getStoredUserCredentials();
-
-        String token = storedUserCredentials.getToken();
-        String storeName = storedUserCredentials.getRepo();
-
-        spiceManager.execute(
-            new UploadedAppsRequest(token, storeName, createMd5StringList(modelList)),
-            new RequestListener<UploadedAppsJson>() {
-              @Override public void onRequestFailure(SpiceException spiceException) {
-                spiceException.printStackTrace();
-              }
-
-              @Override public void onRequestSuccess(UploadedAppsJson uploadedAppsJson) {
-                System.out.println("request was successful");
-
-                List<UploadedAppsJson.DataList.App> remoteAppsList =
-                    uploadedAppsJson.datalist.getList();
-
-                for (int i = 0; i < uploadedAppsJson.datalist.getList()
-                    .size(); i++) {
-                  listOfAppsInStore.add(new UploadedApp(remoteAppsList.get(i)
-                      .getFile()
-                      .getPackageName()
-                      .getName(), remoteAppsList.get(i)
-                      .getFile()
-                      .getVercode()));
-                }
-                updateStoredUploadedApps(listOfAppsInStore);
-              }
-            });
-      }
-    };
-  }
-
-  private void updateStoredUploadedApps(List<UploadedApp> listOfAppsInStore) {
-    appsInStorePersister.saveUploadedApps(listOfAppsInStore);
-  }
-
-  private List<String> createMd5StringList(List<Md5AsyncUtils.Model> modelList) {
-    String str = "[";
-
-    for (Md5AsyncUtils.Model model : modelList) {
-      str += model.getMd5sum() + ",";
-    }
-
-    char[] chars = str.toCharArray();
-    chars[str.length() - 1] = ']';
-
-    List<String> strings = new LinkedList<>();
-    for (Md5AsyncUtils.Model model : modelList) {
-      strings.add(model.getMd5sum());
-    }
-
-    return strings;
   }
 
   private boolean isUserLoggedIn() {
