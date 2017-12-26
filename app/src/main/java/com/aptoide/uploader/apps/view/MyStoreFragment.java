491bb29f715b1250dccc2b98a2ece0c9a3614ea0
jdandrade <joao.andrade@aptoide.com>
2017-12-26 09:12:20 +0000

    add sort spinner values and item selection observable.

--- app/src/main/java/com/aptoide/uploader/apps/view/MyStoreFragment.java
+++ app/src/main/java/com/aptoide/uploader/apps/view/MyStoreFragment.java
@@ -7,11 +7,14 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 import com.aptoide.uploader.R;
 import com.aptoide.uploader.UploaderApplication;
 import com.aptoide.uploader.apps.InstalledApp;
 import com.aptoide.uploader.view.android.FragmentView;
+import com.jakewharton.rxbinding2.widget.RxAdapterView;
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
@@ -24,6 +27,7 @@ public class MyStoreFragment extends FragmentView implements MyStoreView {
   private RecyclerView recyclerView;
   private MyAppsAdapter adapter;
   private TextView storeNameText;
+  private Spinner spinner;
 
   public static MyStoreFragment newInstance() {
     return new MyStoreFragment();
@@ -39,6 +43,8 @@ public class MyStoreFragment extends FragmentView implements MyStoreView {
     super.onViewCreated(view, savedInstanceState);
     recyclerView = view.findViewById(R.id.fragment_my_apps_list);
     storeNameText = view.findViewById(R.id.fragment_my_apps_store_name);
+    spinner = view.findViewById(R.id.sort_spinner);
+    prepareSpinner(R.array.sort_spinner_array);
     recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));
     recyclerView.addItemDecoration(new GridDividerItemDecoration(
         getResources().getDimensionPixelSize(R.dimen.apps_grid_item_margin)));
@@ -51,20 +57,19 @@ public class MyStoreFragment extends FragmentView implements MyStoreView {
 
   @Override public void onDestroyView() {
     adapter = null;
+    spinner = null;
     storeNameText = null;
     recyclerView.setAdapter(null);
     recyclerView = null;
     super.onDestroyView();
   }
 
-  @Override public Observable<List<InstalledApp>> submitAppEvent() {
-    return Observable.empty();
+  private void prepareSpinner(int arrayId) {
+    ArrayAdapter<CharSequence> adapter =
+        ArrayAdapter.createFromResource(getActivity(), arrayId, R.layout.spinner_item);
+    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    spinner.setAdapter(adapter);
   }
-
-  @Override public Observable<SortingOrder> orderByEvent() {
-    return null;
-  }
-
 
   @Override public void showApps(@NotNull List<InstalledApp> appsList) {
     adapter.setList(appsList);
@@ -72,5 +77,20 @@ public class MyStoreFragment extends FragmentView implements MyStoreView {
 
   @Override public void showStoreName(@NotNull String storeName) {
     storeNameText.setText(storeName);
+  }
+
+  @Override public Observable<List<InstalledApp>> submitAppEvent() {
+    return Observable.empty();
+  }
+
+  @Override public Observable<SortingOrder> orderByEvent() {
+    return RxAdapterView.itemSelections(spinner)
+        .map(integer -> {
+          if (integer == 0) {
+            return SortingOrder.DATE;
+          } else {
+            return SortingOrder.NAME;
+          }
+        });
   }
 }
