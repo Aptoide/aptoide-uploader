96147197ee4fda63bda38345b732a8c43a0fc1d5
Tiago Pedrinho <rz.tiagopedrinho@gmail.com>
2019-08-30 12:25:05 +0100

    [UP-13] Changed some strings copy.

--- app/src/main/java/com/aptoide/uploader/account/view/CreateStoreFragment.java
+++ app/src/main/java/com/aptoide/uploader/account/view/CreateStoreFragment.java
@@ -145,7 +145,7 @@ public class CreateStoreFragment extends FragmentView implements CreateStoreView
   }
 
   @Override public void showNetworkError() {
-    Toast.makeText(getContext(), getString(R.string.unknown_error), Toast.LENGTH_LONG)
+    Toast.makeText(getContext(), getString(R.string.connection_error_body), Toast.LENGTH_LONG)
         .show();
   }
 
@@ -154,7 +154,7 @@ public class CreateStoreFragment extends FragmentView implements CreateStoreView
   }
 
   @Override public void showErrorStoreAlreadyExists() {
-    Toast.makeText(getContext(), getText(R.string.duplicate_store),
+    Toast.makeText(getContext(), getText(R.string.duplicate_store_error),
         Toast.LENGTH_LONG)
         .show();
   }
