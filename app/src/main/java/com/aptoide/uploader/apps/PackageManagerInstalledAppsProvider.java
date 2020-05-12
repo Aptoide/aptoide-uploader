e23c30529279f69c473e74558f3492817d816e2d
danfbfern <danielfernando.1608@gmail.com>
2020-05-12 21:16:20 +0100

    [MOB-375] Code style

--- app/src/main/java/com/aptoide/uploader/apps/PackageManagerInstalledAppsProvider.java
+++ app/src/main/java/com/aptoide/uploader/apps/PackageManagerInstalledAppsProvider.java
@@ -41,16 +41,15 @@ public class PackageManagerInstalledAppsProvider implements InstalledAppsProvide
   }
 
   @Override public Single<InstalledApp> getInstalledApp(String packageName) {
-    return Single.fromCallable(()->packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA))
-            .map(info -> {
-              PackageInfo packageInfo = packageManager.getPackageInfo(info.packageName, 0);
-              return new InstalledApp(packageInfo.applicationInfo,
-                      info.loadLabel(packageManager)
-                              .toString(), (info.flags & ApplicationInfo.FLAG_SYSTEM) == 1,
-                      packageInfo.packageName, info.sourceDir, packageInfo.lastUpdateTime,
-                      packageInfo.versionCode, false, getMainObb(info.packageName),
-                      getPatchObb(info.packageName));
-            });
+    return Single.fromCallable(
+        () -> packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA))
+        .map(info -> {
+          PackageInfo packageInfo = packageManager.getPackageInfo(info.packageName, 0);
+          return new InstalledApp(packageInfo.applicationInfo, info.loadLabel(packageManager)
+              .toString(), (info.flags & ApplicationInfo.FLAG_SYSTEM) == 1, packageInfo.packageName,
+              info.sourceDir, packageInfo.lastUpdateTime, packageInfo.versionCode, false,
+              getMainObb(info.packageName), getPatchObb(info.packageName));
+        });
   }
 
   private Obb getMainObb(String packageName) {
@@ -60,7 +59,7 @@ public class PackageManagerInstalledAppsProvider implements InstalledAppsProvide
     if (obbDir.isDirectory()) {
       File[] files = obbDir.listFiles();
       if (files != null) {
-      Arrays.sort(files, (f1, f2) -> Long.compare(f2.lastModified(), f1.lastModified()));
+        Arrays.sort(files, (f1, f2) -> Long.compare(f2.lastModified(), f1.lastModified()));
         for (File file : files) {
           if (file.getName()
               .contains("main") && !file.getName()
@@ -82,7 +81,7 @@ public class PackageManagerInstalledAppsProvider implements InstalledAppsProvide
     if (obbDir.isDirectory()) {
       File[] files = obbDir.listFiles();
       if (files != null) {
-      Arrays.sort(files, (f1, f2) -> Long.compare(f2.lastModified(), f1.lastModified()));
+        Arrays.sort(files, (f1, f2) -> Long.compare(f2.lastModified(), f1.lastModified()));
         for (File file : files) {
           String arr[] = file.getName()
               .split("\\.", 2);
